9fe6f6688bf8c2b241139c35910104f74b1c4c70
Lexal-dev <Landoltalexis67@gmail.com>
2025-04-18 02:28:23 +0200

    again

--- src/components/PhaserGame.tsx
+++ src/components/PhaserGame.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import Phaser from 'phaser';
+import * as Phaser from 'phaser';
 import config from '@/phaser/config';
 
 const PhaserGame = () => {
